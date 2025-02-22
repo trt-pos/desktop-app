b11be82617be078e5d26a5e621725680d2528edf
Borja Castellano <borjacastellano1@gmail.com>
2025-02-22 12:08:53 +0100

    log rotation

--- app-launcher/src/main.rs
+++ app-launcher/src/main.rs
@@ -1,13 +1,25 @@
-use std::fs::{File, OpenOptions};
+use std::fs;
+use std::fs::OpenOptions;
 use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::process::{Command, Stdio};
 
+const MAX_LOG_SIZE: u64 = 5 * 1024 * 1024;
+
 fn main() {
     let app_dir = get_app_dir();
 
-    let stdout_path = Path::new(&app_dir).join("trt-output.log");
-    let stderr_path = Path::new(&app_dir).join("trt-error.log");
+    let home_dir = std::env::var(if cfg!(windows) { "USERPROFILE" } else { "HOME" })
+        .expect("Failed to get user HOME");
+
+    let log_dir = Path::new(&home_dir).join(".round-table");
+    fs::create_dir_all(&log_dir).expect("Failed to create log directory");
+
+    let stdout_path = log_dir.join("trt-output.log");
+    let stderr_path = log_dir.join("trt-error.log");
+
+    rotate_log(&stdout_path);
+    rotate_log(&stderr_path);
 
     let stdout_file = OpenOptions::new()
         .create(true)
@@ -29,6 +41,16 @@ fn main() {
         .exec();
 
     println!("Error: {:?}", err);
+}
+
+fn rotate_log(log_path: &Path) {
+    if let Ok(metadata) = fs::metadata(log_path) {
+        if metadata.len() >= MAX_LOG_SIZE {
+            let rotated_log = log_path.with_extension("log.old");
+            let _ = fs::remove_file(&rotated_log);
+            fs::rename(log_path, &rotated_log).expect("log couldn't be rotated");
+        }
+    }
 }
 
 fn get_app_dir() -> String {
