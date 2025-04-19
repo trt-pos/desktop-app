f5d41672ce6fe4e86d16f6b8cbb4cf00f3503472
Borja Castellano <borjacastellano1@gmail.com>
2025-04-19 17:02:11 +0200

    entity form behaviour improved

--- src/main/java/org/lebastudios/theroundtable/dialogs/EntityFormDialogController.java
+++ src/main/java/org/lebastudios/theroundtable/dialogs/EntityFormDialogController.java
@@ -1,12 +1,22 @@
 package org.lebastudios.theroundtable.dialogs;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.NonNull;
+import lombok.SneakyThrows;
 import org.lebastudios.theroundtable.controllers.FormPaneController;
 import org.lebastudios.theroundtable.database.Database;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class EntityFormDialogController<T> extends FormDialogController<T>
 {
+    private boolean persisted;
+    
+    @SneakyThrows
     public EntityFormDialogController(@NonNull FormPaneController<T> formPaneController, @NonNull T object)
     {
         super(formPaneController, object);
@@ -28,8 +38,29 @@ public class EntityFormDialogController<T> extends FormDialogController<T>
         {
             return Database.getInstance().connectTransactionWithBool(session ->
             {
-                session.persist(objectBuilded);
+                Consumer<T> f = persisted ? session::merge : session::persist;
+                f.accept(objectBuilded);
             });
         };
+
+        Field[] fields = object.getClass().getDeclaredFields();
+        Field fieldId = Arrays.stream(fields)
+                .parallel()
+                .filter(field -> field.isAnnotationPresent(Id.class))
+                .findFirst().orElseThrow();
+        String capitalizedName = fieldId.getName().substring(0, 1).toUpperCase() + fieldId.getName().substring(1);
+        Method idGetter = object.getClass().getMethod("get" + capitalizedName);
+        Object objectId = idGetter.invoke(object);
+
+        persisted = objectId != null
+                && Database.getInstance().connectQuery(s -> s.get(object.getClass(), objectId) != null);
+    }
+
+    @Override
+    protected void initialize()
+    {
+        super.initialize();
+        
+        deleteButton.setVisible(persisted);
     }
 }
