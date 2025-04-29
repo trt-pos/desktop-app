2ad44f237591c98956fdba343b78f682b9d40200
Borja Castellano <borjacastellano1@gmail.com>
2025-04-29 23:03:50 +0200

    added new messages in the welcome screen

--- trt-installer/crates/bin/src/gui/steps/welcome.rs
+++ trt-installer/crates/bin/src/gui/steps/welcome.rs
@@ -1,6 +1,6 @@
 use crate::gui::app::Message;
 use crate::gui::steps::Step;
-use iced::{widget, Element, Task};
+use iced::{Element, Task, widget};
 use widget::column;
 
 #[derive(Default)]
@@ -21,16 +21,19 @@ impl Step for WelcomeStep {
 
     fn view(&self) -> Element<Message> {
         column![
-            widget::text("Through this application you will be able to configure and install The Round Table, the open source POS software!").align_x(iced::Alignment::Center),
-        ].padding(5)
+            widget::text("Thank you for choosing The Round Table! The best free and open source POS software in the market")
+            .align_x(iced::Alignment::Center),
+            widget::text("Through this wizard you will be able to configure and install The Round Table, the open source POS software!")
+            .align_x(iced::Alignment::Center),
+            widget::text("Get ready to start your journey with us!")
+        ].spacing(5)
+         .padding(5)
          .into()
     }
 
-    fn update(&mut self, message: Message) -> Task<Message> {
+    fn update(&mut self, _: Message) -> Task<Message> {
         Task::none()
     }
-
-
 
     fn apply(&self) -> Task<Message> {
         Task::future(async move { Message::NextStep })
