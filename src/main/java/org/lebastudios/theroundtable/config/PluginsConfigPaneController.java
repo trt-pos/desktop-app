6c180fbf8cba0a1d5f4f84ba5877925ce95115bf
Borja Castellano <borjacastellano1@gmail.com>
2025-05-04 13:43:36 +0200

    functional buttons in the PluginConfigPaneController

--- src/main/java/org/lebastudios/theroundtable/config/PluginsConfigPaneController.java
+++ src/main/java/org/lebastudios/theroundtable/config/PluginsConfigPaneController.java
@@ -3,6 +3,8 @@ package org.lebastudios.theroundtable.config;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import org.lebastudios.theroundtable.dialogs.RequestTextDialogController;
+import org.lebastudios.theroundtable.plugins.PluginRepoIntrospector;
 
 public class PluginsConfigPaneController extends ConfigPaneController<PluginsConfigData>
 {
@@ -16,7 +18,8 @@ public class PluginsConfigPaneController extends ConfigPaneController<PluginsCon
     @Override
     public void updateConfigData(PluginsConfigData configData)
     {
-        
+        configData.repos.clear();
+        configData.repos.addAll(reposList.getItems());
     }
 
     @Override
@@ -34,12 +37,30 @@ public class PluginsConfigPaneController extends ConfigPaneController<PluginsCon
     @FXML
     public void addRepo(ActionEvent actionEvent)
     {
-        
+        new RequestTextDialogController(url -> reposList.getItems().add(url),
+                 "https://repo.domain.com", "New plugin repository",
+                url ->
+                {
+                    try
+                    {
+                        return new PluginRepoIntrospector(url).ping();
+                    }
+                    catch (Exception exception)
+                    {
+                        return false;
+                    }
+                }).setOwner(this.getStage())
+                .instantiate();
     }
 
     @FXML
     public void removeRepo(ActionEvent actionEvent)
     {
-        
+        String selected = reposList.getSelectionModel().getSelectedItem();
+
+        if (selected == null) return;
+        ;
+
+        reposList.getItems().remove(selected);
     }
 }
