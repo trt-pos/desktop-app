67ebd363081be95e53d55df520bbf4590374c7bb
Borja Castellano <borjacastellano1@gmail.com>
2025-05-07 18:49:33 +0200

    to currency string for BigDecimals

--- src/main/java/org/lebastudios/theroundtable/maths/BigDecimalOperations.java
+++ src/main/java/org/lebastudios/theroundtable/maths/BigDecimalOperations.java
@@ -1,5 +1,7 @@
 package org.lebastudios.theroundtable.maths;
 
+import org.lebastudios.theroundtable.locale.LocaleManager;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
@@ -9,17 +11,22 @@ public class BigDecimalOperations
     {
         return dividend.divide(divisor, 2, RoundingMode.CEILING);
     }
-    
+
     public static BigDecimal dividePrecise(BigDecimal dividend, BigDecimal divisor)
     {
         return dividend.divide(divisor, 10, RoundingMode.FLOOR);
     }
-    
+
     public static String toString(BigDecimal value)
     {
         return round(value).toString();
-    } 
-    
+    }
+
+    public static String toCurrencyString(BigDecimal value)
+    {
+        return toString(value) + " " + LocaleManager.getInstance().getActualCurrency().symbol();
+    }
+
     public static BigDecimal round(BigDecimal value)
     {
         return value.setScale(2, RoundingMode.CEILING);
