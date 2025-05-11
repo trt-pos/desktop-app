b0f1d46de5f86855b2b26fc89e1fbafb7d58bc72
Borja Castellano <borjacastellano1@gmail.com>
2025-05-11 22:33:27 +0200

    PrintTask accepts a PrintService

--- src/main/java/org/lebastudios/theroundtable/printers/PrintTask.java
+++ src/main/java/org/lebastudios/theroundtable/printers/PrintTask.java
@@ -1,9 +1,12 @@
 package org.lebastudios.theroundtable.printers;
 
 import com.github.anastaciocintra.escpos.EscPos;
+import com.github.anastaciocintra.output.PrinterOutputStream;
 import lombok.Setter;
+import org.lebastudios.theroundtable.dialogs.ExceptionDialogController;
 import org.lebastudios.theroundtable.tasks.Task;
 
+import javax.print.PrintService;
 import java.io.IOException;
 
 public abstract class PrintTask extends Task<Void>
@@ -17,7 +20,20 @@ public abstract class PrintTask extends Task<Void>
     {
         this.escpos = escPos;
     }
-
+    
+    public PrintTask(PrintService printService)
+    {
+        try
+        {
+            this.escpos = new EscPos(new PrinterOutputStream(printService));
+        }
+        catch (IOException e)
+        {
+            new ExceptionDialogController(e).instantiate(true);
+            throw new RuntimeException(e);
+        }
+    }
+    
     @Override
     protected Void call() throws Exception
     {
