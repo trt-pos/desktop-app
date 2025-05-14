8391ef777dafe1b25582360787693bc39022972a
Borja Castellano <borjacastellano1@gmail.com>
2025-05-14 22:51:41 +0200

    no more FXMLLoader, only compiled views

--- src/main/java/org/lebastudios/theroundtable/controllers/Controller.java
+++ src/main/java/org/lebastudios/theroundtable/controllers/Controller.java
@@ -1,19 +1,14 @@
 package org.lebastudios.theroundtable.controllers;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.lebastudios.theroundtable.locale.Translator;
-import org.lebastudios.theroundtable.logs.Logs;
 import org.lebastudios.theroundtable.plugins.IPlugin;
 import org.lebastudios.theroundtable.plugins.PluginsManager;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.net.URL;
 
 public abstract class Controller<T extends Controller<T>>
 {
@@ -51,42 +46,10 @@ public abstract class Controller<T extends Controller<T>>
 
             this.root = (Node) viewClass.getConstructors()[0].newInstance(this);
             this.initialize();
-            return;
         }
-        catch (ClassNotFoundException ignore) {}
-        catch (InvocationTargetException | InstantiationException | IllegalAccessException e)
+        catch (Exception e)
         {
             throw new RuntimeException(e);
-        }
-
-        loadFXML(true);
-    }
-
-    private void loadFXML(boolean injectController)
-    {
-        try
-        {
-            FXMLLoader fxmlLoader = getFXMLLoader();
-            Translator.getInstance().injectT(fxmlLoader, getBundleClass());
-            
-            fxmlLoader.setController(injectController ? this : null);
-            this.root = fxmlLoader.load();
-            this.controller = fxmlLoader.getController();
-        }
-        catch (IOException e)
-        {
-            if (e.getMessage().contains("Controller value already specified."))
-            {
-                loadFXML(false);
-            }
-            else
-            {
-                Logs.getInstance().log(
-                        "Unexpected error while trying to load the FXML for "
-                                + this.getClass().getName() + ": " + this.getFXML().getFile(),
-                        e
-                );
-            }
         }
     }
     
@@ -114,32 +77,5 @@ public abstract class Controller<T extends Controller<T>>
         return PluginsManager.getInstance().getPluginOf(this.getClass())
                 .orElseThrow()
                 .getClass();
-    }
-
-    public URL getFXML()
-    {
-        Class<?> clazz = getClass();
-        String fxmlNameLowerCamelCase =
-                clazz.getSimpleName().substring(0, 1).toLowerCase() + clazz.getSimpleName().substring(1);
-        String fxmlNameWithoutController = fxmlNameLowerCamelCase.replace("Controller", "");
-        return clazz.getResource(fxmlNameWithoutController + ".fxml");
-    }
-
-    public final FXMLLoader getFXMLLoader()
-    {
-        final var fxml = getFXML();
-        
-        if (fxml == null) 
-        {
-            Logs.getInstance().log(
-                    Logs.LogType.ERROR,
-                    "FXML loader resource is null for the controller " + this.getClass().getName()
-            );
-        }
-        
-        var fxmlLoader = new FXMLLoader(fxml);
-        fxmlLoader.setClassLoader(getClass().getClassLoader());
-
-        return fxmlLoader;
     }
 }
