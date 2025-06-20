09e9fa2350ccac3655cc241374f8f17be5545a88
Borja Castellano <borjacastellano1@gmail.com>
2025-06-20 19:03:27 +0200

    using a custom implementation of table view 2

---
+++ src/main/java/org/lebastudios/theroundtable/components/EditableTableView.java
@@ -0,0 +1,12 @@
+package org.lebastudios.theroundtable.components;
+
+import org.controlsfx.control.tableview2.TableView2;
+
+public class EditableTableView<S> extends TableView2<S>
+{
+    @Override
+    public String getUserAgentStylesheet()
+    {
+        return null;  
+    }
+}
